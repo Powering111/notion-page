7cb2fe7a33c7e35f8ee004c59a01e649efa1de1e
정준태 <50047351+Powering111@users.noreply.github.com>
2023-05-13 13:49:34 +0900

    change page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'c4aea383f27e4515afb6eccb678d66ad',
+  rootNotionPageId: '911b7922298b445a891ab059c2ee1faa',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
